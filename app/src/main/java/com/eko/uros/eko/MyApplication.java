6a5f998c65ff5665f63436ca0675aa3f1d9d75fd
Ukiand1 <ukiand13@gmail.com>
2015-12-05 01:11:45 +0100

    Initial commit

---
+++ app/src/main/java/com/eko/uros/eko/MyApplication.java
@@ -0,0 +1,40 @@
+package com.eko.uros.eko;
+
+import android.app.Application;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by Uros on 0004 04 Dec.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        printHashKey();
+    }
+
+    public void printHashKey(){
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.uros.facebooksample",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("UROS KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+    }
+}
